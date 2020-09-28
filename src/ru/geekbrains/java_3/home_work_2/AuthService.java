603c7f2bb22cb1379509acb77f631353d899c451
OlegKom <oleg465020@yandex.ru>
2020-09-28 20:52:46 +0500

    добавлено дз2 по Java3 Базы данных

---
+++ src/ru/geekbrains/java_3/home_work_2/AuthService.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.java_3.home_work_2;
+
+public interface AuthService {
+    void start();
+    String getNickByLoginPass(String login, String pass);
+    boolean changeNickName(String login, String pass,String nickname);
+    void stop();
+
+}
